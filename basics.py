4babbae6a79c3c47dc74f8b76c84bd2383188c67
slhodak <slhodak@gmail.com>
2020-11-03 19:58:00 -0800

    Display attainments grouped and labeled by age

--- basics.py
+++ basics.py
@@ -6,8 +6,8 @@ import pandas as pandas
 sns.set_theme(style="ticks")
 
 df = pandas.read_csv("data/table-1-1-mf.csv")
-
-sns.relplot(data=df)
+grouped = df.set_index("Age Group")
+sns.relplot(data=grouped, height=5, aspect=2)
 
 plt.show()
 # %%
